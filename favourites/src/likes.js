fbea59f90d9cc2af4eef8ddcd086dce7978dedbe
johnsn27 <nathan.johnson@bbc.co.uk>
2019-07-11 16:36:11 +0100

    Security fix

--- favourites/src/likes.js
+++ favourites/src/likes.js
@@ -19,20 +19,19 @@ class Likes extends React.Component {
   }
 
   getLikes() {
-    var x="", i, currentItem;
+    var x = "", i, currentItem;
     for (i = 0; i < localStorage.length; i++) {
       currentItem = localStorage.key(i);
       if (currentItem === 'button1' || currentItem === 'button2') {
         x = x + localStorage.key(i) + " ";
       }
     }
+    console.log(x);
   }
 
   render() {
     return (
       <div className="outerDiv">
-        <div className="top">
-        </div>
         <div id="button1" className="divbutton">
           <button onClick={() => {
             this.updateLikes("button1")
@@ -47,7 +46,6 @@ class Likes extends React.Component {
           this.getLikes()
         }}>Get Likes</button>
       </div>
-
     );
   }
 }
