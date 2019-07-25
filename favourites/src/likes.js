c7e2bfd92b670398d9e7681692b617ad5b78ccd7
johnsn27 <nathan.johnson@bbc.co.uk>
2019-07-25 11:30:51 +0100

    Multiple buttons

--- favourites/src/likes.js
+++ favourites/src/likes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Btn from './blockButton/blockButton.js';
+import './likes.css'
 
 class Likes extends React.Component {
 
@@ -22,7 +22,7 @@ class Likes extends React.Component {
     var x = "", i, currentItem;
     for (i = 0; i < localStorage.length; i++) {
       currentItem = localStorage.key(i);
-      if (currentItem === 'button1' || currentItem === 'button2') {
+      if (currentItem === 'block1' || currentItem === 'block2') {
         x = x + localStorage.key(i) + " ";
       }
     }
@@ -32,30 +32,25 @@ class Likes extends React.Component {
   render() {
     return (
       <div className="wrapper">
-        <div className="outerDiv">
-          <div className="top">
+        <div id="buttonsWrapper" className="buttonsWrapper">
+          <div id="block1wrapper" className="block1wrapper">
+            <div id="block1" className="divbutton">
+              <button onClick={() => {
+                this.updateLikes("block1")
+              }}>block 1</button>
+            </div>
           </div>
-          <div id="button1" className="divbutton">
-            <button onClick={() => {
-              this.updateLikes("button1")
-            }}>Click Me!</button>
+          <div id="block2wrapper" className="block2wrapper">
+            <div id="block2" className="divbutton">
+              <button onClick={() => {
+                this.updateLikes("block2")
+              }}>block 2</button>
+            </div>
           </div>
-          <button onClick={() => {
-            this.getLikes()
-          }}>Get Likes</button>
         </div>
-        <div className="outerDiv2">
-          <div className="top">
-          </div>
-          <div id="button2" className="divbutton">
-            <button onClick={() => {
-              this.updateLikes("button2")
-            }}>Click Me!</button>
-          </div>
-          <button onClick={() => {
-            this.getLikes()
-          }}>Get Likes</button>
-        </div>
+        <button onClick={() => {
+          this.getLikes()
+        }}>Get Likes</button>
       </div>
     );
   }
