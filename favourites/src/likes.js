74ed6042812567b8e8c9b52fa0466ef690cd782a
johnsn27 <nathan.johnson@bbc.co.uk>
2019-08-08 12:52:06 +0100

    Horizonal rows

--- favourites/src/likes.js
+++ favourites/src/likes.js
@@ -1,4 +1,4 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import LeftSideBarButton from './LeftSideBarButtons/LeftSideBarButton.js';
 import './likes.css';
@@ -6,28 +6,15 @@ import './likes.css';
 class LeftSideBar extends Component {
   render() {
     return (
-      <div>
-        <div className="my-section">
-          <div className="my-section-buttons">
-            <div className="button-border">
-              <div className="my-section-button">
-                <LeftSideBarButton id="account" type="primary" tab-index="1" className="Button" link='' text="My Account">
-                </LeftSideBarButton>
-              </div>
-                <div className="my-section-button">
-                  <LeftSideBarButton id="bookings" type="primary" tab-index="1" className="Button" link='' text="My Bookings">
-                  </LeftSideBarButton>
-                </div>
-                <div className="my-section-button">
-                  <LeftSideBarButton id="watchedRooms" type="primary" tab-index="1" className="Button" link='/watched-rooms' text="My Watched Rooms">
-                  </LeftSideBarButton>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div >
-      );
-    }
+      <div class="flex-container row">
+        <div class="flex-item">1</div>
+        <div class="flex-item">2</div>
+        <div class="flex-item">3</div>
+        <div class="flex-item">4</div>
+        <div class="flex-item">5</div>
+      </div>
+    );
   }
+}
 
-  export default LeftSideBar;
+export default LeftSideBar;
