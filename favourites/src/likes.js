fce31f413e3e80a862ac8f4291f809d44d87dc93
johnsn27 <nathan.johnson@bbc.co.uk>
2019-08-08 15:30:30 +0100

    Update Likes method working

--- favourites/src/likes.js
+++ favourites/src/likes.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import LeftSideBarButton from './LeftSideBarButtons/LeftSideBarButton.js';
 import './likes.css';
 
 class LeftSideBar extends Component {
+
+  updateLikes(blockType) {
+    if(typeof(Storage) !== "undefined") {
+      if (localStorage.getItem(blockType)) {
+        const currentValue = Number(localStorage.getItem(blockType));
+        const newValue = currentValue + 1;
+        localStorage.setItem(blockType, newValue);
+      } else {
+        localStorage.setItem(blockType, 1);
+      }
+      console.log("You have clicked the button " + localStorage.getItem(blockType) + " times.");
+    } else {
+      console.log("Sorry, your browser does not support web storage...");
+    }
+  }
+
   render() {
     return (
-      <div class="flex-container row">
-        <div class="flex-item">"Hover over me!</div>
-        <div id="hover">Stuff shown on hover</div>
-        <div class="flex-item">"Hover over me!</div>
-        <div id="hover">Stuff shown on hover</div>
-        <div class="flex-item">"Hover over me!</div>
-        <div id="hover">Stuff shown on hover</div>
+      <div className="flex-container row">
+        <div className="flex-item" onClick={() => this.updateLikes('headline')}>Headline</div>
+        <div className="flex-item" onClick={() => this.updateLikes('text')}>Text</div>
+        <div className="flex-item" onClick={() => this.updateLikes('another one')}>Hover over me!</div>
       </div>
     );
   }
