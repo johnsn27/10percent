4e378979993ef213a8031bf114b1fcc77f294e98
johnsn27 <nathan.johnson@bbc.co.uk>
2019-08-08 16:40:37 +0100

    WIP sorting algorithm

--- favourites/src/likes.js
+++ favourites/src/likes.js
@@ -4,7 +4,7 @@ import './likes.css';
 class LeftSideBar extends Component {
 
   updateClicks(blockType) {
-    if(typeof(Storage) !== "undefined") {
+    if (typeof (Storage) !== "undefined") {
       if (localStorage.getItem(blockType)) {
         const currentValue = Number(localStorage.getItem(blockType));
         const newValue = currentValue + 1;
@@ -18,7 +18,33 @@ class LeftSideBar extends Component {
     }
   }
 
+  getOrder(blocks) {
+    let firstBlockClicks
+    let firstBlock
+    blocks.forEach(function (block) {
+      if (firstBlockClicks) {
+        if (Number(localStorage.getItem(block)) > Number(firstBlockClicks)) {
+          console.log(block, localStorage.getItem(block))
+          firstBlock = block
+        }
+      }
+      else {
+        firstBlockClicks = localStorage.getItem(block)
+      }
+    });
+    var index = blocks.indexOf(firstBlock);
+    if (index > -1) {
+      blocks.splice(index, 1);
+    }
+    return [blocks, firstBlock];
+  }
+
   render() {
+    const blocks = ['text', 'headline', 'another one'];
+    const firstBlock = this.getOrder(blocks);
+    console.log(firstBlock);
+    const secondBlock = this.getOrder(blocks);
+    const thirdBlock = this.getOrder(blocks);
     return (
       <div className="flex-container row">
         <div id="headline" className="flex-item" onClick={() => this.updateClicks('headline')}>Headline</div>
